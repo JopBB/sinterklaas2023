a9a965c98b00ad02904f86263991b6ca5b896375
Jop <jopbrandenburg@gmail.com>
2023-11-18 15:19:41 +0100

    articuleren flow uitgewerkt

--- oefenenScript.js
+++ oefenenScript.js
@@ -1,6 +1,9 @@
-mediumGeluidGehoord=false;
+mediumGeluidGehoord = false;
 hardGeluidGehoord = false;
-
+currentSpeechPart = 0;
+speechPart = {};
+opdracht=[];
+fillOpdracht();
 navigator.mediaDevices.getUserMedia({
   audio: true,
   video: false
@@ -27,7 +30,6 @@ navigator.mediaDevices.getUserMedia({
       const average = arraySum / array.length;
       const geluid = Math.round(average) 
       colorPids(geluid);
-      console.log(geluid);
        if(geluid>=60){
         hardGeluidGehoord=true;
         showWhenTooSoft.css("display", "none");
@@ -41,7 +43,11 @@ navigator.mediaDevices.getUserMedia({
         showWhenTooSoft.css("display", "none");
         showWhenHardEnough.css("display", "none");
         showWhenLittleBitOfSound.css("display", "inline-block");
-       }else if(geluid >= 10 && !mediumGeluidGehoord && !hardGeluidGehoord){
+       }else if(geluid >= 5 && !mediumGeluidGehoord && !hardGeluidGehoord){
+        showWhenLittleBitOfSound.css("display", "none");
+        showWhenHardEnough.css("display", "none");
+        showWhenTooSoft.css("display", "inline-block");
+       }else if(geluid === 0 && !mediumGeluidGehoord && !hardGeluidGehoord){
         showWhenLittleBitOfSound.css("display", "none");
         showWhenHardEnough.css("display", "none");
         showWhenTooSoft.css("display", "inline-block");
@@ -64,5 +70,29 @@ function colorPids(vol) {
   for (const pid of pidsToColor) {
     // console.log(pid[i]);
     pid.style.backgroundColor = "#69ce2b";
+  }
+}
+
+function fillOpdracht(){
+  speechPart = speechParts[currentSpeechPart];
+  opdracht = speechPart.antwoordPart.split(" ");
+  const originalText = document.getElementById("original__text");
+  originalText.innerHTML = '';
+  for (let i = 0; i < opdracht.length; i++) {
+    const spanNode = document.createElement("span");
+    spanNode.textContent = opdracht[i];
+    originalText.appendChild(spanNode);
+  }
+}
+
+function loadSpeechPart(){
+  if(currentSpeechPart!==3){
+    currentSpeechPart++;
+    showWhenLittleBitOfSound.css("display", "none");
+    showWhenHardEnough.css("display", "none");
+    showWhenTooSoft.css("display", "none");
+    fillOpdracht();
+  }else{
+    alert("van harte gefeliciteerd! Je hebt goed genoeg geoefend!")
   }
 }
